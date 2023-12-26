974659f39aecb468fc010f76772bc70f2f62f1c1
pomelo_hsieh <pomelo_hsieh@asus.com>
2023-12-26 14:51:17 +0800

    Add gpio_fan_test script for Tinker board 3 factory image.
    Change-Id: Ib64515bed891bde88875fea10fe6b2e7d0b13b75
    Signed-off-by: pomelo_hsieh <pomelo_hsieh@asus.com>
    
    Former-commit-id: 2e5cc6f5191faffe6d490d385d16cf90af880b2b

---
+++ overlay-factory/home/linaro/Desktop/factory_tools_Tinker_Board_3/script/gpio_fan_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+$(sudo su -c "echo 15 > /sys/class/gpio/export")
+$(sudo su -c "echo out > /sys/class/gpio/gpio15/direction")
+$(sudo su -c "echo 1 > /sys/class/gpio/gpio15/value")
+sleep 1
+result1=$(cat /sys/class/gpio/gpio15/value)
+#echo "result1: $result1"
+if [ "$result1" == "1" ]; then
+	$(sudo su -c "echo 0 > /sys/class/gpio/gpio15/value")
+	sleep 1
+	result2=$(cat /sys/class/gpio/gpio15/value)
+	#echo "result2: $result2"
+	if [ "$result2" == "0" ]; then
+		echo "PASS"
+		$(sudo su -c "echo 15 > /sys/class/gpio/unexport")
+	else
+		echo "FAIL"
+	fi
+else
+	echo "FAIL"
+fi
