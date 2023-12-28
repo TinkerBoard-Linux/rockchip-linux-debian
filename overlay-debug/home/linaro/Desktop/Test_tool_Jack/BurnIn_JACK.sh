808a4617fe9b8efdc8c792abae22a1e8a600e50a
yi-hsin_hung <yi-hsin_hung@asus.com>
2023-12-28 11:09:46 +0800

    Test tool: Modify the Burnin and System test file name
    Change-Id: Ia0d60150b3ae77a41a58f1c1b94efa3e50bada54
    Signed-off-by: yi-hsin_hung <yi-hsin_hung@asus.com>
    
    Former-commit-id: 476cb9328e5d1cb30559647c75d252df5d0a73b7

---
+++ overlay-debug/home/linaro/Desktop/Test_tool_Jack/BurnIn_JACK.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+version=4.12.20231228
+
+cd ./BurnIn_test
+sudo ./BurnIn.sh JACK
+
---
+++ overlay-debug/home/linaro/Desktop/Test_tool_Jack/System_test_JACK_stop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+version=3.0.20231228
+
+cd ./System_test
+sudo ./System_test.sh JACK 4
+
