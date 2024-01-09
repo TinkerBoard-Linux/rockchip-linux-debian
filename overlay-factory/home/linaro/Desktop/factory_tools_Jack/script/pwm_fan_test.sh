24dcc5043324e0d8c96b5e09b063d0711ef5484a
pomelo_hsieh <pomelo_hsieh@asus.com>
2024-01-09 16:00:10 +0800

    Change hwmon0 to hwmon4 for factory fan test for Jack project.
    Change-Id: I280666e40c5a737f8c31faabc75ab1d0749f19b7
    Signed-off-by: pomelo_hsieh <pomelo_hsieh@asus.com>
    
    Former-commit-id: c049901e207909c3f46583212c86303d08162204

--- overlay-factory/home/linaro/Desktop/factory_tools_Jack/script/pwm_fan_test.sh
+++ overlay-factory/home/linaro/Desktop/factory_tools_Jack/script/pwm_fan_test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-name=$(sudo su -c "cat /sys/class/hwmon/hwmon0/name")
+name=$(sudo su -c "cat /sys/class/hwmon/hwmon4/name")
 
 if [ "$name" == "pwmfan" ]; then
-	$(sudo su -c "echo 100 > /sys/class/hwmon/hwmon0/pwm_dutycycle")
-	$(sudo su -c "echo 10000000 > /sys/class/hwmon/hwmon0/pwm_period")
+	$(sudo su -c "echo 100 > /sys/class/hwmon/hwmon4/pwm_dutycycle")
+	$(sudo su -c "echo 10000000 > /sys/class/hwmon/hwmon4/pwm_period")
 	sleep 1
-	pwm_period=$(sudo su -c "cat /sys/class/hwmon/hwmon0/pwm_period")
+	pwm_period=$(sudo su -c "cat /sys/class/hwmon/hwmon4/pwm_period")
 	pwm_freq=$((1000000000/$pwm_period))
 	echo "PWM Freq:" $pwm_freq
 	sleep 1
-	rpm=$(sudo su -c "cat /sys/class/hwmon/hwmon0/fan_input")
+	rpm=$(sudo su -c "cat /sys/class/hwmon/hwmon4/fan_input")
 	echo "RPM:" $rpm
 	freq=$(($rpm/30))
 	echo "Freq:" $freq
@@ -20,5 +20,5 @@ if [ "$name" == "pwmfan" ]; then
 		echo "FAIL, error freq number"
 	fi
 else
-	echo "FAIL, can't get hwmon0 name"
+	echo "FAIL, can't get pwmfan name"
 fi
