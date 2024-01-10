47d444d80a88a6e780b6dbfc399075fe58971bcc
pomelo_hsieh <pomelo_hsieh@asus.com>
2024-01-10 15:42:51 +0800

    Modify pwm fan test script for factory.
    Change-Id: Id9f7b4accb1494b109a50d3b44edefc23781b699
    Signed-off-by: pomelo_hsieh <pomelo_hsieh@asus.com>
    
    Former-commit-id: 9ef10b864f74149916da8701bae82e4c9653da49

--- overlay-factory/home/linaro/Desktop/factory_tools_Jack/script/pwm_fan_test.sh
+++ overlay-factory/home/linaro/Desktop/factory_tools_Jack/script/pwm_fan_test.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-name=$(sudo su -c "cat /sys/class/hwmon/hwmon4/name")
-
-if [ "$name" == "pwmfan" ]; then
-	$(sudo su -c "echo 100 > /sys/class/hwmon/hwmon4/pwm_dutycycle")
-	$(sudo su -c "echo 10000000 > /sys/class/hwmon/hwmon4/pwm_period")
-	sleep 1
-	pwm_period=$(sudo su -c "cat /sys/class/hwmon/hwmon4/pwm_period")
-	pwm_freq=$((1000000000/$pwm_period))
-	echo "PWM Freq:" $pwm_freq
-	sleep 1
-	rpm=$(sudo su -c "cat /sys/class/hwmon/hwmon4/fan_input")
-	echo "RPM:" $rpm
-	freq=$(($rpm/30))
-	echo "Freq:" $freq
-	if [[ $freq -ge 95 ]] && [[ $freq -le 105 ]]; then
-		echo "PASS"
-	else
-		echo "FAIL, error freq number"
+for path in /sys/class/hwmon/hwmon*; do
+	if [[ "pwmfan" == $(sudo su -c "cat $path/name") ]]; then
+		#echo "hwmon name:" $(sudo su -c "cat $path/name")
+		#echo "hwmon pathc:" $path
+		$(sudo su -c "echo 100 > $path/pwm_dutycycle")
+		$(sudo su -c "echo 10000000 > $path/pwm_period")
+		sleep 1
+		pwm_period=$(sudo su -c "cat $path/pwm_period")
+		pwm_freq=$((1000000000/$pwm_period))
+		echo "PWM Freq:" $pwm_freq
+		sleep 1
+		rpm=$(sudo su -c "cat $path/fan_input")
+		#echo "RPM:" $rpm
+		freq=$(($rpm/30))
+		echo "Receive Freq:" $freq
+		if [[ $freq -ge 95 ]] && [[ $freq -le 105 ]]; then
+			echo "PASS"
+			exit
+		else
+			echo "FAIL, error freq out of spec"
+			exit
+		fi
 	fi
-else
-	echo "FAIL, can't get pwmfan name"
-fi
+done
+echo "FAIL, can't get hwmon path for pwmfan"
