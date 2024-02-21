e2be6fa24eec71edf7e92f4dd2628242c0979adb
Tzuhsuan_Chen <Tzuhsuan_Chen@asus.com>
2024-02-21 16:05:46 +0800

    factory: touch: Add the touch test tool for tb3.
    Change-Id: Ie1fed87bcd2a3506bfeb51bb673c73eb28f6c3f1
    
    Former-commit-id: 86a0063d9056961859c2b53dc71018857c045090

---
+++ overlay-factory/home/linaro/Desktop/factory_tools_Tinker_Board_3/script/touch_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+TOUCH_INPUT_EVENT=/dev/input/event2
+
+if [ ! -e ${HID_INPUT_EVENT} ]; then
+	echo "FAIL"
+	exit 1
+fi
+
+while read -t 5 -r line; do
+	CODE=`echo $line | awk -F "," '{print $3}'`
+	VALUE=`echo $line | awk -F "," '{print $4}'`
+	if [ "$CODE" == " code 53 (ABS_MT_POSITION_X)" ]; then
+		ABS_X=$VALUE
+	elif [ "$CODE" == " code 54 (ABS_MT_POSITION_Y)" ]; then
+		ABS_Y=$VALUE
+	fi
+
+	if [ -n "$ABS_X" ] && [ -n "$ABS_Y" ]; then
+		x=`echo $ABS_X | awk -F " " '{print $2}'`
+		y=`echo $ABS_Y | awk -F " " '{print $2}'`
+		echo "$x,$y"
+		exit 0
+	fi
+done < <(evtest --grab ${TOUCH_INPUT_EVENT})
+
+echo "FAIL"
+exit 1
