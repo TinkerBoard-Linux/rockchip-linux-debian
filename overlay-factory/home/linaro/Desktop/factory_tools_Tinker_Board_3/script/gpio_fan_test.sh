da534bc9eabeba1f8f8e70fdcfdbee73456f198f
pomelo_hsieh <pomelo_hsieh@asus.com>
2024-03-27 16:56:18 +0800

    Modify to turn off the fan power to 4 seconds on factory.
    Change-Id: I5c3b3220d7ed3237c84df9ae3f941cadc8418b68
    Signed-off-by: pomelo_hsieh <pomelo_hsieh@asus.com>
    
    Former-commit-id: 34b48352bad885184940a449b1bb4530babbdd03

--- overlay-factory/home/linaro/Desktop/factory_tools_Tinker_Board_3/script/gpio_fan_test.sh
+++ overlay-factory/home/linaro/Desktop/factory_tools_Tinker_Board_3/script/gpio_fan_test.sh
@@ -3,7 +3,7 @@
 $(sudo su -c "echo 123 >  /sys/class/gpio/export")
 $(sudo su -c "echo out > /sys/class/gpio/gpio123/direction")
 $(sudo su -c "echo 0 > /sys/class/gpio/gpio123/value")
-sleep 0.5
+sleep 4
 $(sudo su -c "echo 1 >  /sys/class/gpio/gpio123/value")
 sleep 0.5
 $(sudo su -c "echo 15 > /sys/class/gpio/export")
