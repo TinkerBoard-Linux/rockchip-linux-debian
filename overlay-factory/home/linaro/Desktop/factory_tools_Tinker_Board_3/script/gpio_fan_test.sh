21a0f61c68bdc5d1667276db9b55cf3b11481cb4
pomelo_hsieh <pomelo_hsieh@asus.com>
2024-06-05 15:53:38 +0800

    Modify gpio fan test script for Tinker board 3 ER.
    Change-Id: If1fa0665070ec9dea87a9d3884f9a6bda551021c
    Signed-off-by: pomelo_hsieh <pomelo_hsieh@asus.com>
    
    Former-commit-id: 6ed4a0bcf400ceda1bfadd9b3cf407de7e836d54

--- overlay-factory/home/linaro/Desktop/factory_tools_Tinker_Board_3/script/gpio_fan_test.sh
+++ overlay-factory/home/linaro/Desktop/factory_tools_Tinker_Board_3/script/gpio_fan_test.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-$(sudo su -c "echo 123 >  /sys/class/gpio/export")
-$(sudo su -c "echo out > /sys/class/gpio/gpio123/direction")
-$(sudo su -c "echo 0 > /sys/class/gpio/gpio123/value")
-sleep 4
-$(sudo su -c "echo 1 >  /sys/class/gpio/gpio123/value")
-sleep 0.5
-$(sudo su -c "echo 15 > /sys/class/gpio/export")
-$(sudo su -c "echo out > /sys/class/gpio/gpio15/direction")
-$(sudo su -c "echo 0 > /sys/class/gpio/gpio15/value")
-sleep 1
-result1=$(cat /sys/class/gpio/gpio15/value)
+$(sudo su -c "echo 0 > /sys/class/hwmon/hwmon2/fan_power")
+sleep 5
+result1=$(cat /sys/class/hwmon/hwmon2/fan_power)
 #echo "result1: $result1"
 if [ "$result1" == "0" ]; then
-	$(sudo su -c "echo 1 > /sys/class/gpio/gpio15/value")
-	sleep 3
-	result2=$(cat /sys/class/gpio/gpio15/value)
+	$(sudo su -c "echo 1 > /sys/class/hwmon/hwmon2/fan_power")
+	sleep 2
+	result2=$(cat /sys/class/hwmon/hwmon2/fan_power)
 	#echo "result2: $result2"
 	if [ "$result2" == "1" ]; then
-		echo "PASS"
-		$(sudo su -c "echo 0 > /sys/class/gpio/gpio15/value")
-		$(sudo su -c "echo 15 > /sys/class/gpio/unexport")
-		$(sudo su -c "echo 123 > /sys/class/gpio/unexport")
+		$(sudo su -c "echo 1 > /sys/class/hwmon/hwmon2/fan_speed")
+		sleep 5
+		result3=$(cat /sys/class/hwmon/hwmon2/fan_speed)
+		#echo "result3: $result3"
+		if [ "$result3" == "1" ]; then
+			$(sudo su -c "echo 0 > /sys/class/hwmon/hwmon2/fan_speed")
+			sleep 5
+			result4=$(cat /sys/class/hwmon/hwmon2/fan_speed)
+			#echo "result4: $result4"
+			if [ "$result4" == "0" ]; then
+				echo "PASS"
+			else
+				echo "FAIL"
+			fi
+		else
+			echo "FAIL"
+		fi
 	else
 		echo "FAIL"
 	fi
