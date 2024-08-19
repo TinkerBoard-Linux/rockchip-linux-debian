3b2249b9219b3de0f0944932074287995d96ccb3
Tzuhsuan_Chen <Tzuhsuan_Chen@asus.com>
2024-08-19 17:00:51 +0800

    tools: Add the factory tools for tinker board 3 rv.
    cloned from factory_tools_Tinker_Board_3
    
    Change-Id: I8a40c8821e6e48f359595fc8e399a9878cc0964f
    
    Former-commit-id: 0d27ace596849afcba0afc2ad53fcc39b615d377

---
+++ overlay-factory/home/linaro/Desktop/factory_tools_Tinker_Board_3_RV/script/usb_tool_test.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ $# -ne 4 ]; then
+	echo "usage: sudo ./usb_tool_test.sh <u2 port number> <u3 port number> <typeC port number> <test times>"
+	exit -1
+fi
+
+u2port=$1
+u3port=$2
+tcport=$3
+count=$4
+
+echo off > /sys/kernel/debug/usb/ehci/fd800000.usb/hub_vbus
+echo off > /sys/kernel/debug/usb/fd000000.dwc3/hub_vbus
+
+sleep 1
+
+echo on > /sys/kernel/debug/usb/ehci/fd800000.usb/hub_vbus
+echo on > /sys/kernel/debug/usb/fd000000.dwc3/hub_vbus
+
+sleep 3
+
+cardCount=$(lsusb|grep "iCreate Technologies"|wc -l)
+if [ "$cardCount" != "$u2port" ]; then
+	echo "FAIL, connected test card number not equal to u2 port number"
+	exit -1
+fi
+
+for((i=1; i<=count; i++))
+do
+	sudo UsbTool /switch -speed SS -enable 1
+	sleep 1
+	sudo UsbTool /tdc -hc $u2port -sc $u3port -bhc $tcport -bsc $tcport -nu30s 0 -d3f 0 -ccst 1\
+		> /var/log/usb_test_card.log
+
+	if [ "$?" == "0" ]; then
+		if [ "$i" != "$count" ]; then
+			sleep 2
+		fi
+	else
+		echo "FAIL"
+		exit -1
+	fi
+done
+
+echo "PASS"
+
