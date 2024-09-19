1fb416cf57e64222f6f44ed8cf0b2616f6ea178a
yi-hsin_hung <yi-hsin_hung@asus.com>
2024-09-19 09:22:59 +0800

    factory: remove the wifi_rssi script tool GW log
    Change-Id: Idb6cc983620c46854dbc5a3af8f671d87d8e324f
    Signed-off-by: yi-hsin_hung <yi-hsin_hung@asus.com>
    
    Former-commit-id: da365c55afd4051d7765555fd1c655809068e69e

--- overlay-factory/home/linaro/Desktop/factory_tools_Tinker_Board_3N/script/wifi_rssi.sh
+++ overlay-factory/home/linaro/Desktop/factory_tools_Tinker_Board_3N/script/wifi_rssi.sh
@@ -32,7 +32,7 @@ if [ "$interface_wifi" != "" ]; then
     sleep 1
 
     GW=$(/sbin/route -n | grep "${interface_wifi}" | grep UG | awk '{printf $2}')
-    echo "GW=$GW"
+#    echo "GW=$GW"
     if [[ -n "$GW" ]]; then
         sudo ping $GW -w 100 -c 1 > /dev/null 2>&1
 	if [ ! $? -eq 0 ]; then
