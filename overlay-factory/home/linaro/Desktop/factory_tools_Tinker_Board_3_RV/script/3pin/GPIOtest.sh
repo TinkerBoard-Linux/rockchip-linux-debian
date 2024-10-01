e18661d0833466382ff754b53976d594f0ed67c6
locker_kao <locker_kao@asus.com>
2024-10-01 11:12:07 +0800

    3Pin: Factory: update test script
    Change-Id: Id25a584e66c435fa83f6002760cac0d578f392f4
    Signed-off-by: locker_kao <locker_kao@asus.com>
    
    Former-commit-id: 1783325b651461fffcd99d80291a3630eb82a5a3

--- overlay-factory/home/linaro/Desktop/factory_tools_Tinker_Board_3_RV/script/3pin/GPIOtest.sh
+++ overlay-factory/home/linaro/Desktop/factory_tools_Tinker_Board_3_RV/script/3pin/GPIOtest.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Define GPIO pin arrays (corrected numbering based on your comments)
-gpio_a=(11 8 16 147 83 81 108 150 106 100 101 17 99 23)
-gpio_b=(12 85 146 149 82 84 107 22 105 76 102 75 117 20)
+gpio_a=(16 17 20)
+gpio_b=(17 20 16)
+gpio_c=(20 16 17)
 
 # Initialize test result
 result="PASS"
@@ -23,26 +24,30 @@ function gpio_selftest() {
   local index="$1"
   local output_pin="$2"
   local input_pin="$3"
+  local input_pin_2="$4"
 
   # Set pin directions and initial output value
   echo "in" > "/sys/class/gpio/gpio${input_pin}/direction"
+  echo "in" > "/sys/class/gpio/gpio${input_pin_2}/direction"
   echo "out" > "/sys/class/gpio/gpio${output_pin}/direction"
   echo "0" > "/sys/class/gpio/gpio${output_pin}/value"
 
   # Read pin values for low and high output states
   local FromStatusL=$(cat "/sys/class/gpio/gpio${output_pin}/value")
   local ToStatusL=$(cat "/sys/class/gpio/gpio${input_pin}/value")
+  local ToStatusL_2=$(cat "/sys/class/gpio/gpio${input_pin_2}/value")
 
   echo "out" > "/sys/class/gpio/gpio${output_pin}/direction"
   echo "1" > "/sys/class/gpio/gpio${output_pin}/value"
 
   local FromStatusH=$(cat "/sys/class/gpio/gpio${output_pin}/value")
   local ToStatusH=$(cat "/sys/class/gpio/gpio${input_pin}/value")
+  local ToStatusH_2=$(cat "/sys/class/gpio/gpio${input_pin_2}/value")
 
   # Print status information and check for successful test
 #  echo "IN=PIN#${input_pin}, OUT=PIN#${output_pin}"
   #echo "FromStatusL=${FromStatusL} ToStatusL=${ToStatusL} FromStatusH=${FromStatusH} ToStatusH=${ToStatusH}"
-  if [[ $FromStatusL -eq 0 && $ToStatusL -eq 0 && $FromStatusH -eq 1 && $ToStatusH -eq 1 ]]; then
+  if [[ $FromStatusL -eq 0 && $ToStatusL -eq 0 && $ToStatusL_2 -eq 0 && $FromStatusH -eq 1 && $ToStatusH -eq 1 && $ToStatusH_2 -eq 1 ]]; then
 #    echo "PASS"
     result="PASS"
   else
@@ -54,16 +59,22 @@ function gpio_selftest() {
 for ((i = 0; i < ${#gpio_a[@]}; i++)); do
   gpio_reset "${gpio_a[$i]}"
   gpio_reset "${gpio_b[$i]}"
+  gpio_reset "${gpio_c[$i]}"
 
-  gpio_selftest "$i" "${gpio_a[$i]}" "${gpio_b[$i]}"
+  gpio_selftest "$i" "${gpio_a[$i]}" "${gpio_b[$i]}" "${gpio_c[$i]}"
   if [ "$result" == "FAIL" ]; then
-    echo "FAIL, ${gpio_a[$i]} to ${gpio_b[$i]}"
+    echo "FAIL, ${gpio_a[$i]} to ${gpio_b[$i]} and ${gpio_c[$i]}"
     break
   fi
 
-  gpio_selftest "$i" "${gpio_b[$i]}" "${gpio_a[$i]}"
+  gpio_selftest "$i" "${gpio_b[$i]}" "${gpio_a[$i]}" "${gpio_c[$i]}"
   if [ "$result" == "FAIL" ]; then
-    echo "FAIL, ${gpio_b[$i]} to ${gpio_a[$i]}"
+    echo "FAIL, ${gpio_b[$i]} to ${gpio_a[$i]} and ${gpio_c[$i]}"
+    break
+  fi
+  gpio_selftest "$i" "${gpio_c[$i]}" "${gpio_a[$i]}" "${gpio_b[$i]}"
+  if [ "$result" == "FAIL" ]; then
+    echo "FAIL, ${gpio_c[$i]} to ${gpio_a[$i]} and ${gpio_b[$i]}"
     break
   fi
 done
