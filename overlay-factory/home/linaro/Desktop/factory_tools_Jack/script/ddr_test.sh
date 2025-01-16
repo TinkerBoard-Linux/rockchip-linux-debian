27c5c6aae28d92fb3547b85cec4421a2d295c5f5
Tzuhsuan_Chen <Tzuhsuan_Chen@asus.com>
2025-01-16 15:57:21 +0800

    factory: Fix the ddr test tool.
    If dmc is disabled, the result will be failed.
    
    Change-Id: I43f68b8ae5337f5c37d898774eb3b7dd60f6b7a4
    
    Former-commit-id: 37b134f95e4f1e84ebb436a106d08fc2c9f7247c

--- overlay-factory/home/linaro/Desktop/factory_tools_Jack/script/ddr_test.sh
+++ overlay-factory/home/linaro/Desktop/factory_tools_Jack/script/ddr_test.sh
@@ -2,8 +2,12 @@
 
 MEM_TOTAL=$(free -m | awk '/^Mem:/{print $2}')
 
-echo performance > /sys/class/devfreq/dmc/governor
-DRAM_CLOCK=$(cat /sys/class/devfreq/dmc/cur_freq 2> /dev/null)
+if [ -d "/sys/class/devfreq/dmc/" ]; then
+    echo performance > /sys/class/devfreq/dmc/governor
+    DRAM_CLOCK=$(cat /sys/class/devfreq/dmc/cur_freq 2> /dev/null)
+else
+    DRAM_CLOCK=$(cat /sys/kernel/debug/clk/clk_scmi_ddr/clk_rate 2> /dev/null)
+fi
 
 if [ -z $DRAM_CLOCK ]; then
     echo "FAIL"
